3cbe6b0db429dccd6387a0366575b95abedf4091
KyuBinKwon <kyubinkwon829@gmail.com>
2017-11-15 21:02:20 -0500

    good bwop

--- arrangeCirclesRunner.m
+++ arrangeCirclesRunner.m
@@ -1,4 +1,10 @@
-x = localSearch(@arrangeCircles, @wideScaleRandomNoisePairProposal, (1:20),0.1,0,20,10,0.01)
+x = localSearch(@arrangeCircles, @wideScaleRandomNoisePairProposal, (1:20),0.1,1,20,10,0.01); 
 
 % chose wideScaleRandomNoisePairProposal because we want to change one
-% 'circle' at a time 
+% 'circle' position at a time 
+% epsilon of 0.1 because we want to allow for a small room of 'bad' moves 
+% lowest x and highest x set to 1,20 to keep it within the input range 
+
+% experimented with different epsilon values - 0.6, 0.1, 0.01 etc. 
+% 0.6 often gave a big number of around 43, and there was little
+% discernible difference between 0.1 and 0.01.
--- bwopRunner.m
+++ bwopRunner.m
@@ -1,1 +1,20 @@
-% Empty file! Put whatever you want here
+initialX = ones(1,40); 
+i = 1; 
+val = 1;  
+while i < 25
+    initialX(i) = val; 
+    initialX(i+1) = val; 
+    i = i + 2; 
+    val = val * -1; 
+end 
+sign = 1; 
+while i < 40
+    initialX(i) = val; 
+    initialX(i+1) = val; 
+    i = i + 2; 
+    sign = sign * -1; 
+    val = rand() * sign; 
+end
+
+%x = localSearch(@bwop, @wideScaleRandomNoisePairProposal, initialX,.01,-1,1,10,0.01); 
+x = localSearch(@bwop, @bwopProposal, initialX, .001,-1,1,10,0.05); 
